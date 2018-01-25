33a4334bb6f9e2e1140f2842cab854e306fae810
MACAW\Rocky <rocky.tjon.a.sin@macaw.nl>
2018-01-25 14:10:23 +0100

    setup firebase

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+// Start writing Firebase Functions
+// https://firebase.google.com/functions/write-firebase-functions
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
 //# sourceMappingURL=index.js.map
